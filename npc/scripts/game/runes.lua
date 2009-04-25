d12e1894d7f4692cbe8b3676a6d97e4411a462c2
Renan Cunha <renan1bara@gmail.com>
2009-04-25 02:58:55 +0000

    * olá
    git-svn-id: svn://darghos.com.br/darghos/data/rev840@624 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- npc/scripts/game/runes.lua
+++ npc/scripts/game/runes.lua
@@ -8,6 +8,9 @@ function onCreatureDisappear(cid) 			npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)			npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()					npcHandler:onThink()					end
 
+
+
+
 npcHandler:setMessage(MESSAGE_GREET, "Greetings |PLAYERNAME|. Welcome to my shop, say {trade} to see what I got to offer..") 
 
 	local shopModule = ShopModule:new()
@@ -44,17 +47,8 @@ shopModule:addBuyableItem({'chameleon'}, 2291, 210, 1, 'chameleon rune')
 shopModule:addBuyableItem({'blank rune'}, 2260, 10, 1, 'blank Rune')
 shopModule:addBuyableItem({'blank rune'}, 2286, 10, 2, 'Poison Bomb')
 shopModule:addBuyableItem({'desintegrate'}, 2310, 80,  3, 'desintegreate rune')
-
-shopModule:addBuyableItemContainer({'bp slhp'}, 2000, 8704, 400, 1, 'backpack of small health potions')
-shopModule:addBuyableItemContainer({'bp hp'}, 2000, 7618, 900, 1, 'backpack of health potions')
-shopModule:addBuyableItemContainer({'bp mp'}, 2001, 7620, 1000, 1, 'backpack of mana potions')
-shopModule:addBuyableItemContainer({'bp shp'}, 2000, 7588, 2000, 1, 'backpack of strong health potions')
-shopModule:addBuyableItemContainer({'bp smp'}, 2001, 7589, 1600, 1, 'backpack of strong mana potions')
-shopModule:addBuyableItemContainer({'bp ghp'}, 2000, 7591, 3800, 1, 'backpack of great health potions')
-shopModule:addBuyableItemContainer({'bp gmp'}, 2001, 7590, 2400, 1, 'backpack of great mana potions')
-shopModule:addBuyableItemContainer({'bp gsp'}, 1999, 8472, 3800, 1, 'backpack of great spirit potions')
-shopModule:addBuyableItemContainer({'bp uhp'}, 2000, 8473, 6200, 1, 'backpack of ultimate health potions')
-shopModule:addBuyableItemContainer({'bp ap'}, 2002, 8474, 2000, 1, 'backpack of antidote potions')
+shopModule:addBuyableItem({'vial of blood'}, 20002, 50,  1, 'vial of blood')
+shopModule:addBuyableItem({'vial of oil'}, 20011, 50,  1, 'vial of oil')
 
 shopModule:addBuyableItem({'wand of vortex', 'vortex'}, 2190, 500, 'wand of vortex')
 shopModule:addBuyableItem({'wand of dragonbreath', 'dragonbreath'}, 2191, 1000, 'wand of dragonbreath')
@@ -73,39 +67,7 @@ shopModule:addBuyableItem({'springsprout rod', 'springsprout'}, 8912, 18000, 'sp
 shopModule:addBuyableItem({'underworld rod', 'underworld'}, 8910, 22000, 'underworld rod')
 
 
-function creatureSayCallback(cid, type, msg)
-	if(not npcHandler:isFocused(cid)) then
-		return false
-	end
 
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
-	local items = {[1] = 2190, [2] = 2182, [5] = 2190, [6] = 2182}
-
-	if(msgcontains(msg, 'first rod') or msgcontains(msg, 'first wand')) then
-		if(isSorcerer(cid) or isDruid(cid)) then
-			if(getPlayerStorageValue(cid, 30002) == -1) then
-				selfSay('So you ask me for a {' .. getItemNameById(items[getPlayerVocation(cid)]) .. '} to begin your advanture?', cid)
-				talkState[talkUser] = 1
-			else
-				selfSay('What? I have already gave you one {' .. getItemNameById(items[getPlayerVocation(cid)]) .. '}!', cid)
-			end
-		else
-			selfSay('Sorry, you aren\'t a druid either a sorcerer.', cid)
-		end
-	elseif(msgcontains(msg, 'yes')) then
-		if(talkState[talkUser] == 1) then
-			doPlayerAddItem(cid, items[getPlayerVocation(cid)], 1)
-			selfSay('Here you are young adept, take care yourself.', cid)
-			setPlayerStorageValue(cid, 30002, 1)
-		end
-		talkState[talkUser] = 0
-	elseif(msgcontains(msg, 'no') and isInArray({1}, talkState[talkUser]) == TRUE) then
-		selfSay('Ok then.', cid)
-		talkState[talkUser] = 0
-	end
-
-	return true
-end
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
